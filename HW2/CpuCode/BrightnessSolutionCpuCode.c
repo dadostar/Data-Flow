d419558ac39ce81d1740b21086eac085e43e6a0d
Davide C <dceneda@live.com>
2018-05-11 13:45:36 +0200

    HW2+ minor modifications

---
+++ HW2/CpuCode/BrightnessSolutionCpuCode.c
@@ -0,0 +1,37 @@
+/**
+ * Document: MaxCompiler Training (maxcompiler-training.pdf)
+ * Chapter: 1
+ * Exercise Solution: 1
+ * Summary:
+ *   Adjusts the brightness of an image by adding a constant
+ *   value to every pixel.
+ */
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "Maxfiles.h"
+#include <MaxSLiCInterface.h>
+
+#include "ppmIO.h"
+
+int main(void)
+{
+	printf("Loading image.\n");
+	int32_t *inImage;
+	int width = 0, height = 0;
+	loadImage("lena.ppm", &inImage, &width, &height, 1);
+
+	int dataSize = width * height * sizeof(int32_t);
+	// Allocate a buffer for the output image
+	int32_t *outImage = malloc(dataSize);
+
+	printf("Running Kernel.\n");
+	BrightnessSolution(width * height, 100, inImage, dataSize, outImage, dataSize);
+
+	printf("Saving image.\n");
+	writeImage("lena_brightness.ppm", outImage, width, height, 1);
+
+	printf("Exiting\n");
+	return 0;
+}
---
+++ HW2/KernelCode/BrightnessSolutionKernel.maxj.c
@@ -0,0 +1,47 @@
+/**
+ * Document: MaxCompiler Training (maxcompiler-training.pdf)
+ * Chapter: 1
+ * Exercise Solution: 1
+ * Summary:
+ *   Kernel that takes a scalar input and adds it to every pixel
+ *   to adjust the brightness of an image.
+ */
+package brightness;
+
+import com.maxeler.maxcompiler.v2.kernelcompiler.Kernel;
+import com.maxeler.maxcompiler.v2.kernelcompiler.KernelParameters;
+import com.maxeler.maxcompiler.v2.kernelcompiler.types.base.DFEVar;
+
+class BrightnessSolutionKernel extends Kernel {
+
+	BrightnessSolutionKernel(KernelParameters parameters) {
+		super(parameters);
+		DFEVar inImage = io.input("inImage", dfeInt(32));
+		
+		
+		int height = 540;
+		int width = 720;
+
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                
+                //mark the pixels as "2" if the neighbor is black
+                DFEVar v <== inImage[i * width + j];
+                inImage[ (i-1) * width + j] <== i > 0 && inImage[(i-1) * width + j] == 0 && v == 1 ? 2 : 1;
+                inImage[ i * width + j - 1] <== j > 0 && inImage[i * width + j - 1] == 0 && v == 1 ? 2 : 1;
+                inImage[ (i+1) * width + j] <== (i + 1) < height && inImage[(i + 1) * width + j] == 0 && v == 1? 2 : 1;
+                inImage[ i * width + j + 1] <== (j + 1) < width && inImage[i * width + j + 1] == 0 && v == 1? 2 : 1;
+            }
+        }
+        
+        // if the value is 2 then I make the pixel black to enlarge the black line thickness
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                inImage[i * width + j] <== inImage[i * width + j] == 2? 1 : 0;
+                
+            }
+        }
+   
+		io.output("outImage", inImage, result.getType());
+	}
+}
