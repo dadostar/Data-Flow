543d9ce67b47b7c4193aef47b0530ad32fb855aa
Davide C <dceneda@live.com>
2018-05-11 14:27:52 +0200

    minor changes

--- HW2/KernelCode/BrightnessSolutionKernel.maxj.c
+++ HW2/KernelCode/BrightnessSolutionKernel.maxj.c
@@ -11,37 +11,40 @@ package brightness;
 import com.maxeler.maxcompiler.v2.kernelcompiler.Kernel;
 import com.maxeler.maxcompiler.v2.kernelcompiler.KernelParameters;
 import com.maxeler.maxcompiler.v2.kernelcompiler.types.base.DFEVar;
+import com.maxeler.maxcompiler.v2.kernelcompiler.types.base.DFEType;
 
 class BrightnessSolutionKernel extends Kernel {
 
-	BrightnessSolutionKernel(KernelParameters parameters) {
-		super(parameters);
-		DFEVar inImage = io.input("inImage", dfeInt(32));
-		
-		
-		int height = 540;
-		int width = 720;
+    BrightnessSolutionKernel(KernelParameters parameters) {
+        super(parameters);
+        DFEVar inImage = io.input("inImage", dfeInt(32));
+        
+        DFEType doubleType=dfeFloat(11,53);
+        DFEType singleType=dfeFloat(8,24);
+        
+        int height = 540;
+        int width = 720;
 
         for (int i=0; i<height; i++){
             for (int j=0; j<width; j++){
                 
                 //mark the pixels as "2" if the neighbor is black
-                DFEVar v <== inImage[i * width + j];
-                inImage[ (i-1) * width + j] <== i > 0 && inImage[(i-1) * width + j] == 0 && v == 1 ? 2 : 1;
-                inImage[ i * width + j - 1] <== j > 0 && inImage[i * width + j - 1] == 0 && v == 1 ? 2 : 1;
-                inImage[ (i+1) * width + j] <== (i + 1) < height && inImage[(i + 1) * width + j] == 0 && v == 1? 2 : 1;
-                inImage[ i * width + j + 1] <== (j + 1) < width && inImage[i * width + j + 1] == 0 && v == 1? 2 : 1;
+                DFEVar v = inImage[i * width + j];
+                inImage[ (i-1) * width + j] <== (i > 0 & inImage[(i-1) * width + j] === constant.var(singleType,0) & v === constant.var(singleType,1)) ? constant.var(singleType,2) : constant.var(singleType,1);
+                inImage[ i * width + j - 1] <== j > 0 & inImage[i * width + j - 1] === constant.var(singleType,0) & v === constant.var(singleType,1) ? constant.var(singleType,2) : constant.var(singleType,1);
+                inImage[ (i+1) * width + j] <== (i + 1) < height & inImage[(i + 1) * width + j] === constant.var(singleType,0) & v === constant.var(singleType,1)? constant.var(singleType,2) : constant.var(singleType,1);
+                inImage[ i * width + j + 1] <== (j + 1) < width & inImage[i * width + j + 1] === constant.var(singleType,0) & v === constant.var(singleType,1)? constant.var(singleType,2) : constant.var(singleType,1);
             }
         }
         
         // if the value is 2 then I make the pixel black to enlarge the black line thickness
         for (int i=0; i<height; i++){
             for (int j=0; j<width; j++){
-                inImage[i * width + j] <== inImage[i * width + j] == 2? 1 : 0;
+                inImage[i * width + j] <== inImage[i * width + j] === constant.var(singleType,2)? constant.var(singleType,1) : constant.var(singleType,0);
                 
             }
         }
    
-		io.output("outImage", inImage, result.getType());
-	}
+        io.output("outImage", inImage, inImage.getType());
+    }
 }
